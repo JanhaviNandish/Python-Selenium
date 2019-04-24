ce932ed4f8361199d54608a875687684168a5f17
Janhavi Nandish <46499964+JanhaviNandish@users.noreply.github.com>
2019-04-25 00:14:19 +0530

    Create linktext.py

---
+++ linktext.py
@@ -0,0 +1,19 @@
+import time
+
+from selenium import webdriver
+
+chrome = webdriver.Chrome(executable_path="F:\SeleniumComponents\chromedriver")
+chrome.maximize_window()
+chrome.get(url="https://magento.com/")
+links = chrome.find_elements_by_tag_name("a")
+print("the link text name is:")
+
+for element in links:
+    link_text_name = element.text
+    print(link_text_name)
+
+    if link_text_name == 'My Account':
+        element.click()
+        break
+time.sleep(3)
+chrome.close()
