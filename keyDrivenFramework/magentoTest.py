f4c4fff8d8fd94514489bbdc7f67c93c2dc2aeee
Janhavi Nandish <46499964+JanhaviNandish@users.noreply.github.com>
2019-09-30 20:19:22 +0530

    Key Driven Framework

---
+++ keyDrivenFramework/magentoTest.py
@@ -0,0 +1,30 @@
+import time
+
+from jproperties import Properties
+from selenium import webdriver
+
+chrome = webdriver.Chrome(executable_path="F:\SeleniumComponents\chromedriver")
+chrome.maximize_window()
+chrome.implicitly_wait(time_to_wait=20)
+chrome.get(url="https://magento.com/")
+p = Properties()
+f = p.load("magento.properties")
+myacc = f.get('myacc')
+print(myacc)
+email = f.get('email')
+print(email)
+pwd = f.get('password')
+print(pwd)
+login = f.get('login')
+print(login)
+logout = f.get('logout')
+print(logout)
+chrome.find_element_by_xpath(xpath = myacc).click()
+chrome.find_element_by_xpath(xpath = email).send_keys("nitinmanjunath1991@gmail.com")
+chrome.find_element_by_xpath(xpath = pwd).send_keys("Welcome123")
+time.sleep(3)
+chrome.find_element_by_xpath(xpath = login).click()
+time.sleep(3)
+chrome.find_element_by_xpath(xpath = logout).click()
+time.sleep(3)
+chrome.close()
